9f25761159a5dcfc9ec81e44987ea4dd1149a25a
gabicfa <gabicfa1996@gmail.com>
2018-12-03 21:12:10 -0200

    edge

--- Aula24/parte2.cu
+++ Aula24/parte2.cu
@@ -7,25 +7,26 @@
 
 #include "imagem.h"
 
+#define MAX(y,x) (y>x?y:x)    // Calcula valor maximo
+#define MIN(y,x) (y<x?y:x)    // Calcula valor minimo
+
 using namespace std;
 
 /* Rotina para somar dois vetores na GPU */ 
-__global__ void blur(unsigned char *input, unsigned char *output,  int height, int width){
+__global__ void blur(unsigned char *in, unsigned char *out, int rowStart, int rowEnd, int colStart, int colEnd){
     int i=blockIdx.x * blockDim.x + threadIdx.x;
     int j=blockIdx.y * blockDim.y + threadIdx.y;
-    double media;
-    if (i< height && j< width) {
-        if(i==0 || i==height-1){
-            media = 0;
+    int di, dj;    
+    if (i< rowEnd && j< colEnd) {
+        int min = 256;
+        int max = 0;
+        for(di = MAX(rowStart, i - 1); di <= MIN(i + 1, rowEnd - 1); di++) {
+            for(dj = MAX(colStart, j - 1); dj <= MIN(j + 1, colEnd - 1); dj++) {
+               if(min>in[di*(colEnd-colStart)+dj]) min = in[di*(colEnd-colStart)+dj];
+               if(max<in[di*(colEnd-colStart)+dj]) max = in[di*(colEnd-colStart)+dj]; 
+            }
         }
-        else if(j == 0 || j == width-1){
-            media = 0;
-        }
-        else{
-            media = (input[i*width+j]+input[(i-1)*width + j-1]+input[(i-1)*width + j]+input[(i-1)*width + j+1]+input[(i-1)*width + j-1]+input[i*width+ j-1]+input[i*width+ j+1]+input[(i+1)*width + j-1]+input[(i+1)*width + j]+input[(i+1)*width + j+1])/9;
-            // int media = (input[i][j-1]+input[i][j+1]+input[i][j]+input[i+1][j-1]+input[i+1][j+1]+input[i+1][j]+input[i-1][j-1]+input[i-1][j+1]+input[i-1][j])/9;
-        }
-        output[i*width+j] = media;
+        out[i*(colEnd-colStart)+j] = max-min;
     }
 }
 
@@ -46,10 +47,10 @@ int main() {
     thrust::device_vector<unsigned char> V2_d(saida->pixels, saida->pixels + saida->total_size );
 
     
-    dim3 dimGrid (ceil(img->rows/36), ceil(img->cols/36),1);
-    dim3 dimBlock(36, 36, 1);
+    dim3 dimGrid (ceil(img->rows/16), ceil(img->cols/16),1);
+    dim3 dimBlock(16, 16, 1);
 
-    blur<<<dimGrid,dimBlock>>>(thrust::raw_pointer_cast(V1_d.data()), thrust::raw_pointer_cast(V2_d.data()), img->rows, img->cols);
+    blur<<<dimGrid,dimBlock>>>(thrust::raw_pointer_cast(V1_d.data()), thrust::raw_pointer_cast(V2_d.data()), 0, img->rows, 0, img->cols);
 
     thrust::host_vector<unsigned char> V3_h(V2_d);
     for(int i = 0; i != V3_h.size(); i++) {

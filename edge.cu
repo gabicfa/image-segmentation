41d881f3f56ffae882b4444fe08b903f9fc10e52
gabicfa <gabicfa1996@gmail.com>
2018-12-04 09:13:45 -0200

    edges

--- edge.cu
+++ edge.cu
@@ -12,8 +12,7 @@
 
 using namespace std;
 
-/* Rotina para somar dois vetores na GPU */ 
-__global__ void blur(unsigned char *in, unsigned char *out, int rowStart, int rowEnd, int colStart, int colEnd){
+__global__ void edge(unsigned char *in, unsigned char *out, int rowStart, int rowEnd, int colStart, int colEnd){
     int i=blockIdx.x * blockDim.x + threadIdx.x;
     int j=blockIdx.y * blockDim.y + threadIdx.y;
     int di, dj;    
@@ -33,31 +32,24 @@ __global__ void blur(unsigned char *in, unsigned char *out, int rowStart, int ro
 /* Programa cria dois vetores e soma eles em GPU */
 int main() {
 
-    cout << "inicio" << '\n';
 
     imagem *img = read_pgm("mona.pgm");
-
-    cout << "lido" << '\n';
-
     imagem *saida = new_image(img->rows, img->cols);
-
-    cout << "saida" << '\n';
 
     thrust::device_vector<unsigned char> V1_d(img->pixels, img->pixels + img->total_size );
     thrust::device_vector<unsigned char> V2_d(saida->pixels, saida->pixels + saida->total_size );
-
     
     dim3 dimGrid (ceil(img->rows/16), ceil(img->cols/16),1);
-    dim3 dimBlock(16, 16, 1);
+    dim3 dimBlofck(16, 16, 1);
 
-    blur<<<dimGrid,dimBlock>>>(thrust::raw_pointer_cast(V1_d.data()), thrust::raw_pointer_cast(V2_d.data()), 0, img->rows, 0, img->cols);
+    edge<<<dimGrid,dimBlock>>>(thrust::raw_pointer_cast(V1_d.data()), thrust::raw_pointer_cast(V2_d.data()), 0, img->rows, 0, img->cols);
 
     thrust::host_vector<unsigned char> V3_h(V2_d);
     for(int i = 0; i != V3_h.size(); i++) {
         saida->pixels[i] = V3_h[i];
     }
-    write_pgm(saida, "blured.pgm");
-    cout << "DONE!" << std::endl;
+
+    write_pgm(saida, "mona_edge.pgm");
 
     return 0;
 }
